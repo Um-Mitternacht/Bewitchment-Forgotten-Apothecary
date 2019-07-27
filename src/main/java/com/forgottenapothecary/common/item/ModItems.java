e45c263602083ed4dd9af3528b5c3a064aaa31a0
Sunconure11 <eyespot7b@aol.com>
2019-07-27 06:16:55 -0400

    Update ModItems.java

--- src/main/java/com/forgottenapothecary/common/item/ModItems.java
+++ src/main/java/com/forgottenapothecary/common/item/ModItems.java
@@ -2,7 +2,10 @@ package com.forgottenapothecary.common.item;
 
 import com.forgottenapothecary.Forgottenapothecary;
 import com.forgottenapothecary.common.lib.LibItemName;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.LoaderException;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 
@@ -19,6 +22,19 @@ public class ModItems {
 	public static void register(final IForgeRegistry<Item> registry) {
 		
 		registry.registerAll(new ItemMod(LibItemName.MOONSTONE));
+	}
+	
+	private static Item itemBlock(Block block) {
+		if (block == null) {
+			throw new LoaderException("[" + Forgottenapothecary.NAME + "] Trying to register an ItemBlock for a null block");
+		}
+		if (block.getRegistryName() == null) {
+			throw new LoaderException("[" + Forgottenapothecary.NAME + "] Trying to register an ItemBlock for a block with null name - " + block.getTranslationKey());
+		}
+		if (block.getRegistryName().toString() == null) {
+			throw new LoaderException("[" + Forgottenapothecary.NAME + "] There's something wrong with the registry implementation of " + block.getTranslationKey());
+		}
+		return new ItemBlock(block).setRegistryName(block.getRegistryName());
 	}
 	
 	public static void init() {
